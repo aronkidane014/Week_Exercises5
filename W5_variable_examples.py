67b71aa4ef5e463b86fd5435da79bca2763fdee6
Your Name <youremail@example.com>
2024-10-21 15:38:44 -0500

    variable_examples

---
+++ W5_variable_examples.py
@@ -0,0 +1,12 @@
+CustomerID= 8
+Customer_name='CourtisRedJones'  
+Gender= 'male'
+Date_of_birth='3/10/1997 '
+Driver_license_number= 'anf17ield97'
+Auto_policy_number= ''
+print(CustomerID)
+print(Customer_name)
+print(Gender)
+print(Date_of_birth)
+print(Driver_license_number)
+print(Auto_policy_number)
