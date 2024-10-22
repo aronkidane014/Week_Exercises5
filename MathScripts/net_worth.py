bdd8921f87c7ccc27a9609b5a6a283c1456ec8ce
Your Name <youremail@example.com>
2024-10-22 14:35:15 -0500

    net_worth and rectangle

---
+++ MathScripts/net_worth.py
@@ -0,0 +1,6 @@
+# to calculat my net worth i need to subtract depts from my assests(income-outcome=networth)
+ 
+assets= 4000
+depts= 2500
+networth= assets - depts
+print("my net worthis" + " " + str(assets))
---
+++ MathScripts/rectangle.py
@@ -0,0 +1,7 @@
+# the formula for area of a rectangle is area= length * width
+l=12
+w=8
+Area= l* w
+#print("The area of the rectangle is" +" "+str(Area))
+
+print(Area)
