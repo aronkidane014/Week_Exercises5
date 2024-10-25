a2a25df260aeb7cf5507d5e757218ca81ce87573
Your Name <youremail@example.com>
2024-10-25 00:42:42 -0500

    samplecollection

---
+++ sampleCollections/movie_list.py
@@ -0,0 +1,11 @@
+movie=["dead pool", "Scarface", "The God Father", "The Mask","Aladdin"]
+#
+# 
+#print(f"Thelist'movies' includes my top {len(movie)} favorite movies.")
+#print(f"This'movies' are my top {len(movie)} favorite movies of all time:{", ".join(movie)}")
+#print(movie) # this prints in the order they written Question4
+#print(sorted(movie)) # this sorts them alphabetical order
+#movie.sort
+#print(sorted(movie)) #it is the same results
+movie.append("Life")
+print(movie)
