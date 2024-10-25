6f96383d982b59efa78c2d6611b58f93d19a683b
Your Name <youremail@example.com>
2024-10-25 15:27:18 -0500

    Candy_Store.py

---
+++ sampleCollections/candy_store.py
@@ -0,0 +1,12 @@
+# Exercise 3B Lab 2
+Candy_Types= ("Gummy Bears", "Lollipops", "Jelly Beans")
+Fruit_Flavors= ("Dragon Fruit", "Passion Pineapple", "Mango Chili")
+Candy_Combo= set()
+Candy_Combo.add((Candy_Types[2],Fruit_Flavors[1])) # question number 3
+Candy_Combo.add((Fruit_Flavors[0],Candy_Types[1])) # question number 3
+print(Candy_Combo) # question number 3
+# Question Number 4 both of them gave the result
+
+print(f"Today’s candy options include:{Candy_Types, Fruit_Flavors}")
+print(f"Today’s candy options include:{Candy_Combo}")
+# question 5 the order of the list changes evertime you run the program
