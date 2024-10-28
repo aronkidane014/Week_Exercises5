43c1a7afa6b52e7ba6812c17b2a0b652e0d83ec8
Your Name <youremail@example.com>
2024-10-28 15:53:04 -0500

    Lab 2 pay_rules

---
+++ ifScripts/pay_rules.py
@@ -0,0 +1,30 @@
+# hours_worked= float(input("hours worked: "))
+# pay_rate= float(input("Pay Rate: 4"))
+# if hours_worked > 40:
+#     regular_pay= hours_worked * 40,
+#     overtime= (hours_worked - 40) * 1.5,
+#     total_pay= regular_pay + overtime
+
+# else:
+#    total_pay= hours_worked * pay_rate
+# print(f"your pay check is {total_pay}")
+     
+hours_worked = float(input("Hours worked: "))
+pay_rate = float(input("Pay Rate: "))
+
+if hours_worked > 40:
+    regular_pay = 40 * pay_rate  
+    overtime = (hours_worked - 40) * pay_rate * 1.5 
+    total_pay = regular_pay + overtime
+    print(f"You worked extra, more money for you! Total pay: ${total_pay:.2f}")
+elif hours_worked < 40:
+    total_pay = hours_worked * pay_rate
+    print(f"Your paycheck is lower due to fewer hours. Total pay: ${total_pay:.2f}")
+elif hours_worked == 40:
+    total_pay = hours_worked * pay_rate
+    print(f"You worked the standard 40 hours. Total pay: ${total_pay:.2f}")
+
+
+
+
+     
