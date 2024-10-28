234c5b52316ecba04cefa9853de4cfd10cf600a0
Your Name <youremail@example.com>
2024-10-28 09:16:23 -0500

    Address_entry FINISHED

--- sampleCollections/address_entry.py
+++ sampleCollections/address_entry.py
@@ -24,3 +24,20 @@ personal_info = f"""\
 
 
 print(personal_info)
+
+personal_info = {
+    "address": "2040 West Town",
+    "city": "Chicago",
+    "state": "Illinois",
+    "zip": "87654321"
+}
+full_name = (("first name", "Arne"), ("last name", "Slot"))
+personal_info.update(dict(full_name))
+personal_info.update({"honorific": "Mr."})
+
+formatted_address = f"""\
+{personal_info['honorific']} {personal_info['first name']} {personal_info['last name']}
+{personal_info['address']}
+{personal_info['city']}, {personal_info['state']} {personal_info['zip']}"""
+
+print(formatted_address)
